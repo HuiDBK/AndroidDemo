7f3cf14b95283292a2b7e01a2f980a6c3529ec37
HuiDBK <2228157440@qq.com>
2019-05-03 18:11:35 +0800

    新增HuiActivity

---
+++ app/src/main/java/com/itcast/test0503/HuiActivity.java
@@ -0,0 +1,13 @@
+package com.itcast.test0503;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HuiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hui);
+    }
+}
